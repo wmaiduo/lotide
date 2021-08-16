e0f3aaa5db8c3088ffc7e55f75d54e061b5a7119
Mai Duo Wu <wmaiduo@gmail.com>
2021-08-16 21:47:13 +0000

    Finished asserEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x2705) + "Assertion Passed :" + actual + " === " + expected);
+  } else console.log(String.fromCodePoint(0x274C) + "Assertion Failed: " + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
