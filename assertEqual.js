e9114fb0bccc0bcfc4151466811542b34cda9440
Mai Duo Wu <wmaiduo@gmail.com>
2021-08-16 21:52:34 +0000

    template literal used

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(String.fromCodePoint(0x2705) + "Assertion Passed :" + actual + " === " + expected);
-  } else console.log(String.fromCodePoint(0x274C) + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(`${String.fromCodePoint(0x2705)} Assertion Passed : ${actual} === ${expected}`);
+  } else console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: actual !== ${expected}`);
 };
 
 // TEST CODE
