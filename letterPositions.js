2d833e51861e463979c82318495ae171ddc18da5
Mai Duo Wu <wmaiduo@gmail.com>
2021-08-19 01:24:18 +0000

    letterPositions.js created

---
+++ letterPositions.js
@@ -0,0 +1,41 @@
+const assertArraysEqual = function (first, second) {
+  if (first.length !== second.length) {
+    console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${first} !== ${second}`);
+    return;
+  } else for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      console.log(`${String.fromCodePoint(0x274C)} Assertion Failed: ${first} !== ${second}`);
+      return;
+    }
+  }
+  console.log(`${String.fromCodePoint(0x2705)} Assertion Passed : ${first} === ${second}`);
+  return;
+}
+
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  } else for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second [i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const letterPositions = function(sentence) {
+  const results = {};
+  let position = 0;
+  for (let char of sentence) {
+    if (char === ' '){
+    } else if (results.hasOwnProperty(char)) {
+      results[char].push(position);
+    } else {
+      results[char] = [position];
+    }
+    position += 1;
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
