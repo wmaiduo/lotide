8be31fb8c94cf56169bb467038a22f029e9af51e
Mai Duo Wu <wmaiduo@gmail.com>
2021-08-24 02:11:42 +0000

    updated middleTest and tailTest so mocha and chai is used

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,8 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-console.log(assertArraysEqual(middle([1,2,3,5,6]),[3]));
-
+describe("#middle", () => {
+  it("should return [3] for [1,2,3,5,6]", () => {
+    assert.deepEqual(middle([1,2,3,5,6]),[3]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,11 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-const a = [1,2];
-const result = tail([0,1,2]);
-assertEqual(result[0],a[0]);
-assertEqual(result[1],a[1]);
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+  it("returns [\"Lighthouse\", \"Labs\"] for [\"Yo Yo\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.deepEqual(tail(["Yo Yo","Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
